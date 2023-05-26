1a35624b51d747275a86f7c4f7e56525e2d8179b
Mojtaba <smo.hayati@gmail.com>
2023-05-26 20:53:30 +0330

    change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import CustomButton from "./components/CustomButton.vue";
 
+// export default {
+//   install: (app, options) => {
+//     app.component("CustomButton", CustomButton);
+//   }
+// };
+
+const install = function(Vue) {
+  Vue.component('CustomButton', CustomButton);
+}
+
+/* istanbul ignore if */
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+export { CustomButton }
+
 export default {
-  install: (app, options) => {
-    app.component("CustomButton", CustomButton);
-  },
-  CustomButton
-};
+  version: '0.0.7',
+  install,
+  CustomButton,
+  customButton: CustomButton
+}
