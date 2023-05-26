d43e8d1800816925f73d486841688dfd298e9e90
Mojtaba <smo.hayati@gmail.com>
2023-05-26 21:04:51 +0330

    change

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,11 +1,28 @@
 <template>
-  <button>
-    <slot />
+  <button
+      style="background: beige;padding: 10px 30px;"
+      :class="{'has-border': hasBorder}"
+  >
+    <slot name="emoji" style="margin-right: 20px"></slot>
+    <slot name="text"></slot>
   </button>
 </template>
+
+<script setup>
+defineProps({
+  hasBorder: {
+    type: Boolean,
+    default: false,
+  }
+})
+</script>
 
 <style scoped>
 button {
   font-size: 20px;
+}
+
+.has-border {
+  border: 4px solid #000;
 }
 </style>
