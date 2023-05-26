df56df0dc22a4c292b7d51fa4276c9fcef16b3b6
Mojtaba <smo.hayati@gmail.com>
2023-05-26 21:05:43 +0330

    change

--- dist/es.js
+++ dist/es.js
@@ -1,26 +1,37 @@
-import { openBlock as _, createElementBlock as r, renderSlot as d } from "vue";
-const i = (t, o) => {
-  const e = t.__vccOpts || t;
-  for (const [s, u] of o)
-    e[s] = u;
-  return e;
-}, l = {};
-function f(t, o) {
-  return _(), r("button", null, [
-    d(t.$slots, "default", {}, void 0, !0)
-  ]);
-}
-const n = /* @__PURE__ */ i(l, [["render", f], ["__scopeId", "data-v-c792b957"]]), c = function(t) {
+import { openBlock as a, createElementBlock as d, normalizeClass as i, renderSlot as s } from "vue";
+const l = (t, e) => {
+  const o = t.__vccOpts || t;
+  for (const [u, c] of e)
+    o[u] = c;
+  return o;
+}, _ = {
+  __name: "CustomButton",
+  props: {
+    hasBorder: {
+      type: Boolean,
+      default: !1
+    }
+  },
+  setup(t) {
+    return (e, o) => (a(), d("button", {
+      style: { background: "beige", padding: "10px 30px" },
+      class: i({ "has-border": t.hasBorder })
+    }, [
+      s(e.$slots, "emoji", { style: "margin-right: 20px" }, void 0, !0),
+      s(e.$slots, "text", {}, void 0, !0)
+    ], 2));
+  }
+}, n = /* @__PURE__ */ l(_, [["__scopeId", "data-v-f02c5c56"]]), r = function(t) {
   t.component("CustomButton", n);
 };
-typeof window < "u" && window.Vue && c(window.Vue);
-const p = {
+typeof window < "u" && window.Vue && r(window.Vue);
+const f = {
   version: "0.0.7",
-  install: c,
+  install: r,
   CustomButton: n,
   customButton: n
 };
 export {
   n as CustomButton,
-  p as default
+  f as default
 };
