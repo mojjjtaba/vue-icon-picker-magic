cc6d69ea6be3c2eca6a9bae2835d9b66c3d56dac
Mojtaba <smo.hayati@gmail.com>
2023-05-26 20:48:38 +0330

    change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import CustomButton from "./components/CustomButton.vue";
 export default {
   install: (app, options) => {
     app.component("CustomButton", CustomButton);
-  }
+  },
+  CustomButton
 };
-
-export { CustomButton }
